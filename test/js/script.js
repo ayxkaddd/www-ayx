3c9df6c1aefc97478d8271a37411b14780b0baa0
ayxdafish <zxcvbqwert390@gmail.com>
2023-02-20 12:35:31 +0200

    thats it for now

--- test/js/script.js
+++ test/js/script.js
@@ -17,3 +17,24 @@ function changeAudio()
         a.innerHTML = "<p class='pointer'>󰕾</p>";
     }
 }
+
+function changeTo1()
+{
+    document.getElementById('w1').innerHTML = "󰮯";
+    document.getElementById('w2').innerHTML = "󰊠";
+    document.getElementById('title').innerHTML = "ayxthefish - Firefox";
+    document.getElementById("body-image").style.visibility="visible";
+}
+
+function changeTo2()
+{
+    document.getElementById('w1').innerHTML = "󰊠";
+    document.getElementById('w2').innerHTML = "󰮯";
+    document.getElementById('title').innerHTML = "~/.config/bspwm";
+}
+
+function changeToEmpty()
+{
+    document.getElementById('title').innerHTML = "ayx - Arch Linux";
+    document.getElementById("body-image").style.visibility="hidden";
+}
